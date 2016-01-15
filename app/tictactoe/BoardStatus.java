0ef822178dc84711d9e66e3054d8df0697ccc2a8
Salaikumar <saravanan1115@gmail.com>
2016-01-15 16:14:45 +0530

    Board Status class implemented. Need to Refactor Duplicate code and test it

--- app/tictactoe/BoardStatus.java
+++ app/tictactoe/BoardStatus.java
@@ -34,16 +34,78 @@ public class BoardStatus {
                 isWinByColumn(Board.botCode) ||
                 isWinByDiagonal(Board.botCode);
     }
-    
+
+    /*
+     * Check if given playerCode is present marks times continuously in a row
+     */
     public boolean isWinByRow(Character playerCode){
+        int count = 0;
+        for (int i = 1 ; i <= board.getSize(); ++i){
+            for (int j=1; j <= board.getSize(); ++j){
+                Character cellValue = board.getCell(i,j);
+                if (count != marks){
+                    if ( cellValue == playerCode)
+                        ++count;
+                    else
+                        count = 0;
+                }
+                else
+                    break;
+            }
+            if (count == marks)
+                return true;
+        }
         return false;
     }
 
     public boolean isWinByColumn(Character playerCode){
+        int count = 0;
+        for (int i = 1 ; i <= board.getSize(); ++i){
+            for (int j=1; j <= board.getSize(); ++j){
+                Character cellValue = board.getCell(j,i);
+                if (count != marks){
+                    if ( cellValue == playerCode)
+                        ++count;
+                    else
+                        count = 0;
+                }
+                else
+                    break;
+            }
+            if (count == marks)
+                return true;
+        }
         return false;
     }
 
     public boolean isWinByDiagonal(Character playerCode){
+
+//       Consider first Diagonal
+        int count = 0;
+        for(int i = 1; i <= board.getSize(); ++i){
+            if (count != marks){
+                if ( playerCode == board.getCell(i,i))
+                    ++count;
+                else
+                    count = 0;
+            }
+            else
+                return true;
+        }
+
+//      Consider second diagonal
+        count = 0;
+        for( int i= board.getSize(); i >0; --i){
+            if (count != marks){
+                if (playerCode == board.getCell(i,i))
+                    ++count;
+                else
+                    count = 0;
+            }
+            else
+                return true;
+        }
+
         return false;
     }
 
